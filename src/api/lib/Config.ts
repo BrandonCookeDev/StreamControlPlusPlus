814828df61891681eac10591492cf3619ac8288a
Brandon Cooke <bcooke@resicap.com>
2019-04-23 15:35:57 -0400

    removed statics in the Files class and replaced it with a static Config file along with a Config class to handle that file

---
+++ src/api/lib/Config.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+import log from '../util/Logger'
+
+export default class Config{
+
+	static CONFIG_FILE: string = path.join(__dirname, '..', '..', '..', 'config', 'config.json')
+
+	static read() : any {
+		log.debug('Config.read called')
+		return JSON.parse(fs.readFileSync(Config.CONFIG_FILE, 'utf8'))
+	}
+
+	static write(data: object) : void{
+		log.debug(`Config.write called: ${JSON.stringify(data)}`)
+		fs.writeFileSync(Config.CONFIG_FILE, JSON.stringify(data, null, 4))
+	}
+
+	static get(propname: string) : any | null{
+		log.debug(`Config.get called: ${propname}`)
+		let content = Config.read()
+		if(content.hasOwnProperty(propname))
+			return content[propname]
+		else return null
+	}
+
+	static set(propname: string, value: string) : void{
+		log.debug(`Config.set called: ${propname} ${value}`)
+		let content = Config.read()
+		content[propname] = value
+		Config.write(content)
+	}
+
+}
--- src/api/lib/Files.ts
+++ src/api/lib/Files.ts
@@ -1,30 +1,31 @@
 import fs from 'fs'
 import path from 'path'
 import log from '../util/Logger'
+import config from './Config'
 
 export default class Files {
 
-	static TEMPLATE_FILE: string | null = null
-	static DATA_FILE: string = 'stream_controll_pp.json'
+	static TEMPLATE_FILE: string | null = config.get("templateFile")
+	static DATA_FILE: string = config.get("dataFile")
 
 	static getTemplateFilepath(){
 		log.debug('Files.getTemplateFilepath called')
-		return Files.TEMPLATE_FILE
+		return config.get("dataFile")
 	}
 
 	static setTemplateFilepath(filepath: string){
 		log.debug(`Files.setTemplateFilepath called [${filepath}]`)
-		Files.TEMPLATE_FILE = filepath
+		config.set("templateFile", filepath)
 	}
 
 	static getDataFilepath(){
 		log.debug('Files.getDataFilepath called')
-		return Files.DATA_FILE
+		return config.get("dataFile")
 	}
 
 	static setDataFilepath(filepath: string){
 		log.debug(`Files.setDataFilepath called [${filepath}]`)
-		Files.DATA_FILE = filepath
+		config.set("dataFile", filepath)
 	}
 
 	static getFromDataFile(): Object{
@@ -34,7 +35,8 @@ export default class Files {
 
 	static writeToDataFile(data: Object){
 		log.debug(`Files.writeToDataFile called. File: ${Files.DATA_FILE}, Data: ${JSON.stringify(data)}`)
-		fs.writeFileSync(Files.DATA_FILE, JSON.stringify(data, null, 4), 'utf8')
+		let dataFile = config.get("dataFile")
+		fs.writeFileSync(dataFile, JSON.stringify(data, null, 4), 'utf8')
 	}
 
 }
