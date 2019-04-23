afdd9e72230b63c894704f8c8f06f7493e55c770
Brandon Cooke <bcooke@resicap.com>
2019-04-23 10:25:14 -0400

    added main logger. shifted some things around, added a ping request

--- src/api/routes/Files.routes.ts
+++ src/api/routes/Files.routes.ts
@@ -1,22 +1,24 @@
 import {Router} from 'express'
 const router = Router()
 import Files from '../lib/Files'
+import log from '../util/Logger'
 
 function setTemplateHandler(req: Express.Request, res: Express.Response){
+	log.info('/files/templateFile POST called')
 	let templateFile = req //.body.template_file
 
 }
 
 function getTemplateHandler(req: Express.Request, res: Express.Response){
-
+	log.info('/files/templateFile GET called')
 }
 
 function getDataHandler(req: Express.Request, res: Express.Response){
-
+	log.info('/files/data GET called')
 }
 
 function setDataHandler(req: Express.Request, res: Express.Response){
-
+	log.info('/files/data POST called [%s]', req)
 }
 
 router.get('/files/templateFile', getTemplateHandler)
