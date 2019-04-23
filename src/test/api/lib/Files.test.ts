3ef8324a74e0f89708a077ccf84f2ad0ffc32d99
Brandon Cooke <bcooke@resicap.com>
2019-04-23 14:56:04 -0400

    added unit test for Files class. Added mocha tester for gulp

---
+++ src/test/api/lib/Files.test.ts
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import path from 'path'
+import chai from 'chai'
+const expect = chai.expect
+
+import Files from '../../../api/lib/Files'
+
+const DEFAULT_TEMPLATE_FILE = null
+const DEFAULT_DATA_FILE = 'stream_controll_pp.json'
+const MANUAL_TEMPLATE_FILE = path.join(__dirname, 'template.cspp')
+const MANUAL_DATA_FILE = path.join(__dirname, 'data.json')
+const MANUAL_DATA = {"p1_name": "hello", "p2_name": "world"}
+
+describe('Files API', function(){
+
+	beforeEach(function(){
+		Files.DATA_FILE = DEFAULT_DATA_FILE
+		Files.TEMPLATE_FILE = DEFAULT_TEMPLATE_FILE
+	})
+
+	afterEach(function(){
+		if(fs.existsSync(DEFAULT_DATA_FILE))
+			fs.unlinkSync(DEFAULT_DATA_FILE)
+	})
+
+	it('should return the correct default data file', function(){
+		expect(Files.getDataFilepath()).to.be.equal(DEFAULT_DATA_FILE)
+	})
+
+	it('should return null for the default template file', function(){
+		expect(Files.getTemplateFilepath()).to.be.null;
+	})
+
+	it('should set the template file correctly', function(){
+		Files.setTemplateFilepath(MANUAL_TEMPLATE_FILE)
+		expect(Files.getTemplateFilepath()).to.be.equal(MANUAL_TEMPLATE_FILE)
+	})
+
+	it('should set the data file correctly', function(){
+		Files.setDataFilepath(MANUAL_DATA_FILE)
+		expect(Files.getDataFilepath()).to.be.equal(MANUAL_DATA_FILE)
+	})
+
+	it('should write the data correctly', function(){
+		expect(fs.existsSync(DEFAULT_DATA_FILE)).to.be.false
+		Files.writeToDataFile(MANUAL_DATA)
+		expect(fs.existsSync(DEFAULT_DATA_FILE)).to.be.true
+		expect(JSON.parse(fs.readFileSync(DEFAULT_DATA_FILE, 'utf8'))).to.deep.equal(MANUAL_DATA)
+	})
+
+})
