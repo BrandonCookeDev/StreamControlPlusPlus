514c00a4f6fab4cbc8e095877d0a98efaf8bcc79
Brandon Cooke <bcooke@resicap.com>
2019-04-23 16:01:41 -0400

    added tslinter, force data file selection to be dir only, enforce naming standard for the data file

--- src/api/lib/Files.ts
+++ src/api/lib/Files.ts
@@ -1,41 +1,39 @@
 import fs from 'fs'
 import path from 'path'
 import log from '../util/Logger'
-import config from './Config'
+import config from '../util/Config'
 
 export default class Files {
 
-	static TEMPLATE_FILE: string | null = config.get("templateFile")
-	static DATA_FILE: string = config.get("dataFile")
-
 	static getTemplateFilepath(){
 		log.debug('Files.getTemplateFilepath called')
-		return config.get("dataFile")
+		return config.get('dataFile')
 	}
 
 	static setTemplateFilepath(filepath: string){
 		log.debug(`Files.setTemplateFilepath called [${filepath}]`)
-		config.set("templateFile", filepath)
+		config.set('templateFile', filepath)
 	}
 
 	static getDataFilepath(){
 		log.debug('Files.getDataFilepath called')
-		return config.get("dataFile")
+		return config.get('dataFile')
 	}
 
 	static setDataFilepath(filepath: string){
 		log.debug(`Files.setDataFilepath called [${filepath}]`)
-		config.set("dataFile", filepath)
+		config.set('dataFile', filepath)
 	}
 
 	static getFromDataFile(): Object{
-		log.debug(`Files.getFromDataFile called. File: ${Files.DATA_FILE}`)
-		return JSON.parse(fs.readFileSync(Files.DATA_FILE, 'utf8'))
+		log.debug(`Files.getFromDataFile called. File: ${config.get('dataFile')}`)
+		let dataFile = config.get('dataFile')
+		return JSON.parse(fs.readFileSync(dataFile, 'utf8'))
 	}
 
 	static writeToDataFile(data: Object){
-		log.debug(`Files.writeToDataFile called. File: ${Files.DATA_FILE}, Data: ${JSON.stringify(data)}`)
-		let dataFile = config.get("dataFile")
+		log.debug(`Files.writeToDataFile called. File: ${config.get('dataFile')}, Data: ${JSON.stringify(data)}`)
+		let dataFile = config.get('dataFile')
 		fs.writeFileSync(dataFile, JSON.stringify(data, null, 4), 'utf8')
 	}
 
