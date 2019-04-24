8bbf3967bcc1c8dfc3d87f045e5a6d8768530cfd
Brandon Cooke <bcooke@resicap.com>
2019-04-24 09:50:22 -0400

    added plugin module content to use regexes to find target areas of key files for wiring. Added an uninstall feature, added init function

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -3,13 +3,45 @@
 const fs = require('fs')
 	, path = require('path')
 	, ncp = require('ncp').ncp
+	, {format} = require('util')
 
+//PATHS
+const API_DIR = path.join(__dirname, '..', 'src', 'api')
 const CLIENT_DIR =  path.join(__dirname, '..', 'client')
+const IMAGES_DIR = path.join(CLIENT_DIR, 'images')
 const VIEWS_DIR = path.join(CLIENT_DIR, 'views')
 const CSS_DIR = path.join(CLIENT_DIR, 'styles')
 const JS_DIR = path.join(CLIENT_DIR, 'js')
+const SPLASH_PAGE_PATH = path.join(CLIENT_DIR, 'splash.html')
+const SETTINGS_PAGE_PATH = path.join(CLIENT_DIR, 'views', 'settings-wrapper.html')
+const MANIFEST_PATH = path.join(__dirname, 'manifest.txt')
+const SPLASH_PAGE_COPY_PATH = format('%s.backup', SPLASH_PAGE_PATH)
+const SETTINS_PAGE_COPY_PATH = format('%s.backup', SETTINGS_PAGE_PATH)
 
-const API_DIR = path.join(__dirname, '..', 'src', 'api')
+//STRINGS
+const NAV_STRING = '<plugins_nav_tabs hidden />'
+const SETTINGS_STRING = '<plugins_setting_tabs hidden />'
+
+//REGEX
+const NAV_REGEX = new RegExp(/\<plugins_nav_tabs hidden \/\>/g)
+const SETTINGS_REGEX = new RegExp(/\<plugins_setting_tabs hidden \/>/g)
+
+//TEMPLATE
+const NAV_TEMPLATE = '<a class="nav-item nav-link tight-spacing" onclick="return hotswap(\'views/%s\')" href="#"><img src="images/%s"></a>'
+const SETTINGS_TEMPLATE = '<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" onclick="return hotswapTab(\'views/%s\')" role="tab" aria-controls="nav-home" aria-selected="true">%s</a>'
+
+function init(){
+	fs.copyFileSync(SETTINGS_PAGE_PATH, SETTINGS_PAGE_COPY_PATH)
+	fs.copyFileSync(SPLASH_PAGE_PATH, SPLASH_PAGE_COPY_PATH)
+}
+
+function initManifest(){
+	fs.writeFile(MANIFEST_PATH, 'SCPP PLUGIN MANIFEST:')
+}
+
+function addToManifest(fileAbsPath){
+	fs.appendFileSync(MANIFEST_PATH, format('\n%s', fileAbsPath))
+}
 
 function register(type, dirname){
 	
@@ -27,13 +59,23 @@ function register(type, dirname){
 			targetExtension = '.css'
 			targetDirectory = JS_DIR
 			break
+		case 'IMAGE':
+			targetExtension = '*'
+			targetDirectory = IMAGES_DIR
+			break
+		default:
+			throw new Error('Unsupported Type: ' + type)
 	}
 
 
 	let dirpath = path.normalize(dirname)
 	fs.readdirSync(dirpath).foreach(file => {
-		if(path.extname(file) === targetExtension)
-			fs.copyFileSync(path.join(dirname, file), path.join(targetDirectory, file))
+		if(path.extname(file) === targetExtension){
+			let fp = path.join(dirname, file)
+			let tp = path.join(targetDirectory, file)
+			fs.copyFileSync(fp, tp)
+			addToManifest(tp)
+		}
 	})
 }
 
@@ -56,9 +98,54 @@ function registerApi(dirname){
 		throw new Error('API Package must include the following: lib/ routes/ util/')
 	
 	return new Promise((resolve, reject) => {
-		ncp(dirname, API_DIR, function(err){
+		ncp(dirpath, API_DIR, function(err){
 			if(err) reject(err)
-			else resolve()
+			else {
+				addToManifest(format('%s/%s', API_DIR, dirname))
+				resolve()
+			}
 		})
 	})
+}
+
+function registerSettingPage(targetFilepath, tabName){
+	let tabElement = format(SETTINGS_TEMPLATE, targetFilepath, tabName)
+	
+	// append nav string in case we need to call this function again
+	tabElement += '\n' + SETTINGS_STRING
+
+	let content = fs.readFileSync(SETTINGS_PAGE_PATH)
+	content.replace(SETTINGS_REGEX, tabElement)
+	fs.writeFileSync(SETTINGS_PAGE_PATH, content)
+}
+
+function registerNavbarLink(targetFilepath, navIconFilepath){
+	let navElement = format(NAV_TEMPLATE, targetFilepath, navIconFilepath)
+	
+	// append nav string in case we need to call this function again
+	navElement += '\n' + NAV_STRING
+
+	let content = fs.readFileSync(SPLASH_PAGE_PATH)
+	content.replace(NAV_REGEX, navElement)
+	fs.writeFileSync(SPLASH_PAGE_PATH, content)
+}
+
+function registerConfigSetting(){
+	
+}
+
+function uninstall(){
+	// delete files registered in the manifest
+	let filesToDelete = fs.readFileSync(MANIFEST_PATH).split('\n')
+	filesToDelete.foreach(absFilepath => {
+		fs.unlinkSync(absFilepath)
+	})
+	
+	// reinstate backups
+	if(fs.existsSync(SPLASH_PAGE_COPY_PATH))
+		fs.copyFileSync(SPLASH_PAGE_COPY_PATH, SPLASH_PAGE_PATH)
+	if(fs.existsSync(SETTINGS_PAGE_COPY_PATH))
+		fs.copyFileSync(SETTINS_PAGE_COPY_PATH, SETTINGS_PAGE_PATH)
+
+	// 
 }
