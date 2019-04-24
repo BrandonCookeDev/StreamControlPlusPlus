242c04d989fd68c05946d6d56e6808e0541bcdfc
Brandon Cooke <bcooke@resicap.com>
2019-04-24 11:36:31 -0400

    updated plugin test code to be operable

--- plugins/test.js
+++ plugins/test.js
@@ -21,6 +21,10 @@ describe('SCPP Plugin Module', function(){
 		setup()
 	})
 
+	beforeEach(function(){
+		plugin.init()
+	})
+
 	afterEach(function(){
 		teardownEach()
 	})
@@ -30,12 +34,61 @@ describe('SCPP Plugin Module', function(){
 	})
 
 	it('should initialize correctly', function(){
-		plugin.init()
+		// init called in beforeEach
 		expect(fs.existsSync(SPLASH_PAGE_COPY_PATH)).to.be.true
 		expect(fs.existsSync(SETTINS_PAGE_COPY_PATH)).to.be.true
 		expect(fs.existsSync(MANIFEST_PATH)).to.be.true
 	})
 
+	it('should add a filepath to the manifest correctly', function(){
+		let p1 = path.join(__dirname, 'test1.png')
+		let p2 = path.join(__dirname, 'test/test3.html')
+		plugin.addToManifest(p1)
+		plugin.addToManifest(p2)
+		let content = fs.readFileSync(MANIFEST_PATH, 'utf8')
+		expect(new RegExp(p1).test(content)).to.be.true
+		expect(new RegExp(p2).test(content)).to.be.true
+	})
+
+	it('should register views correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register styles correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register js files correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register images correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register api packages correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register a new settings page correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register a new nav link correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should register a new config setting correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should uninstall a plugin correctly', function(){
+		throw new Error('not yet implemented')
+	})
+
+	it('should create a new empty plugin correctly', function(){
+		throw new Error('not yet implemented')
+	})
 
 })
 
@@ -82,8 +135,8 @@ function createEmptyPlugin(name){
 	let imagesDirPath = path.join(packageDirPath, 'images')
 	let apiDirPath = path.join(packageDirPath, 'api')
 
-	fs.mkdirSync(name)
-	fs.writeFileSync(installFilePath, INSTALL_FILE_PREFIX);
+	fs.mkdirSync(folderPath)
+	fs.writeFileSync(installFilePath, '');
 	fs.mkdirSync(packageDirPath)
 	fs.mkdirSync(viewsDirPath)
 	fs.mkdirSync(cssDirPath)
