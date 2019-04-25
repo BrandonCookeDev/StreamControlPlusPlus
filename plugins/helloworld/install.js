13d58c1dbeae3fd5d80272ccfb9c60aa23523805
Brandon Cooke <brandoncookedev@gmail.com>
2019-04-25 11:31:59 -0400

    added helloworld plugin package

---
+++ plugins/helloworld/install.js
@@ -0,0 +1,11 @@
+'use strict';
+const path = require('path')
+let plugin = require('../plugin')
+
+const PACKAGE_PATH = path.join(__dirname, 'package')
+
+plugin.registerViews(path.join(PACKAGE_PATH, 'views'))
+plugin.registerStyles(path.join(PACKAGE_PATH, 'styles'))
+plugin.registerJs(path.join(PACKAGE_PATH, 'js'))
+plugin.registerImages(path.join(PACKAGE_PATH, 'images'))
+plugin.registerApi(path.join(PACKAGE_PATH, 'api'))
