c715b67738622d544fe840e832556de46a5dc849
Brandon Cooke <bcooke@resicap.com>
2019-04-25 13:35:07 -0400

    working installation logic

--- main.js
+++ main.js
@@ -1,111 +1,153 @@
 const { app, BrowserWindow } = require('electron')
 require('./dist/api/server'); // launch the api
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+~async function main(){
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  function runGui(){
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the JavaScript object is garbage collected.
+    let win
 
-  // and load the index.html of the app.
-  win.loadFile('client/splash.html')
+    function createWindow () {
+      // Create the browser window.
+      win = new BrowserWindow({ width: 800, height: 600 })
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+      // and load the index.html of the app.
+      win.loadFile('client/splash.html')
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
+      // Open the DevTools.
+      win.webContents.openDevTools()
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+      // Emitted when the window is closed.
+      win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+      })
+    }
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.on('ready', createWindow)
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+      // On macOS it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (win === null) {
+        createWindow()
+      }
+    })
   }
-})
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
+
+  // Plugin Installations
+  const fs = require('fs')
+  const path = require('path')
+  const zlib = require('zlib')
+  const rimraf = require('rimraf')
+  const unzipper = require('unzipper')
+  const scppPlugin = require('./plugins/plugin')
+
+  const PLUGIN_PATH = path.join(__dirname, 'plugins')
+
+  ////////////////////////////////
+  // Error Handler
+  ///////////////////////////////
+  function errHandler(err) {
+    console.error(err)
+    scppPlugin.uninstall()
+    process.exit(1)
   }
-})
+  process.on('exit', errHandler)
+  process.on('SIGINT', errHandler)
+  process.on('uncaughtException', errHandler)
+  process.on('unhandledRejection', errHandler)
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  /**
+   * installPlugins - IIFE
+   * 
+   * This function iterates through the plugins/ folder
+   * and gathers every *.zip file inside of it. It then
+   * unzips this file, runs its expected install.js file,
+   * and then deletes the unzipped folder. This should effectively
+   * install the plugin on-startup every time.
+   */
+  ~async function installPlugins(){
+    console.log('installing pluggins')
+    scppPlugin.init()
 
-// Plugin Installations
-const fs = require('fs')
-const path = require('path')
-const zlib = require('zlib')
-const rimraf = require('rimraf')
-const scppPlugin = require('./plugins/plugin')
+    // get all target zip file absolute paths
+    let pluginZips = []
+    fs.readdirSync(PLUGIN_PATH).forEach(file => {
+      if(path.extname(file) === '.zip'){
+        console.log('found plugin zip: %s', file)
+        pluginZips.push(path.join(PLUGIN_PATH, file))
+      }
+    })
 
-const PLUGIN_PATH = path.join(__dirname, 'plugins')
+    if(pluginZips <= 0){
+      console.log('no plugins to install')
+      return
+    }
+    else console.log('found %d plugins...', pluginZips.length)
 
-////////////////////////////////
-// Error Handler
-///////////////////////////////
-function errHandler(err) {
-  console.error(err)
-  scppPlugin.uninstall()
-  process.exit(1)
-}
-process.on('exit', errHandler)
-process.on('SIGINT', errHandler)
-process.on('uncaughtException', errHandler)
-process.on('unhandledRejection', errHandler)
+    for(let i in pluginZips){
+      let pluginZipPath = pluginZips[i]      
+      let filename = path.basename(pluginZipPath)
+      let zipName = filename.substring(0, filename.length - path.extname(pluginZipPath).length)
+      let zipDest = path.join(PLUGIN_PATH, zipName)
 
-/**
- * installPlugins - IIFE
- * 
- * This function iterates through the plugins/ folder
- * and gathers every *.zip file inside of it. It then
- * unzips this file, runs its expected install.js file,
- * and then deletes the unzipped folder. This should effectively
- * install the plugin on-startup every time.
- */
-~function installPlugins(){
-  console.log('installing pluggins')
+      console.log('unzipping: %s', zipName)
+      await unzip(pluginZipPath, PLUGIN_PATH)
 
-  // get all target zip file absolute paths
-  const pluginPaths = fs.readdirSync(PLUGIN_PATH).map(file => {
-    if(path.extname(file) === '.zip')
-      return path.join(__dirname, file)
-  })
+      console.log('installing: %s', zipName)
+      require(path.join(zipDest, 'install.js'))
 
-  // parse out and store the target resulting directory paths
-  const pluginDirectories = pluginPaths.map(pluginZipPath => 
-    path.join(path.dirname(pluginZipPath), path.basename(pluginZipPath))
-  )
+      console.log('removing dir: %s', zipDest)
+      await recursiveDelete(zipDest)
+    }
 
-  // unzip each target zip file
-  pluginPaths.forEach(pluginZipPath => {
-    zlib.unzipSync(fs.readFileSync(pluginZipPath))
-  })
+    // run the application
+    runGui()
+  }()
+  
+  function unzip(filepath, destPath){
+    return new Promise(function(resolve, reject){
+      fs.createReadStream(filepath)
+        .pipe(unzipper.Extract({path: destPath}))
+        .on('close', () => {
+          console.log('successfully unzipped %s to %s', 
+            filepath, 
+            destPath
+          )
+          return resolve()
+        })
+        .on('error', e => {
+          console.error(e)
+          return reject(e)
+        })
+    })  
+  }
 
-  // run each plugin directory's install.js
-  pluginDirectories.forEach(pluginDir => {
-    require(path.join(pluginDir, 'install.js'))
-  })
-
-  // delete each plugin directory
-  pluginDirectories.forEach(pluginDir => {
-    fs.unlinkSync(pluginDir)
-  })
+  function recursiveDelete(dirpath){
+    return new Promise(function(resolve, reject){
+      rimraf(dirpath, e => {
+        if(e) reject(e)
+        else resolve()
+      })
+    })
+  }
 }()
